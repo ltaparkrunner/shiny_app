f3827271916c7b45831814476dfbb30d06f6d93b
ltaparkrunner <ltaparkrunner@gmail.com>
2026-03-06 03:18:00 +0400

    Added some more layouts.

--- sales/app.py
+++ sales/app.py
@@ -4,6 +4,7 @@ import calendar
 from pathlib import Path
 from shiny import reactive
 from shiny.express import render, input, ui
+from shiny.ui import layout_column_wrap, layout_columns
 from shinywidgets import render_plotly
 
 ui.page_opts(title="Sales Dashboard -- Video 1 of 5", fillable=False)
@@ -15,40 +16,68 @@ def dat():
     df = pandas.read_csv(infile)
     df["order_date"] = pandas.to_datetime(df["order_date"])
     df["month"] = df["order_date"].dt.month_name()
+    df["value"] = df["quantity_ordered"] * df["price_each"]
     return df
 
-with ui.card():  
-    ui.card_header("Top Selling Products")
+# with ui.card():  
+#     ui.card_header("Top Selling Products")
 
-    with ui.layout_sidebar():  
-        with ui.sidebar(bg="#f8f8f8", open='closed'): 
-            ui.input_numeric("n", "Number of Items", 5, min=2, max=20)
+#     with ui.layout_sidebar():  
+#         with ui.sidebar(bg="#f8f8f8", open='closed'): 
+#             ui.input_numeric("n", "Number of Items", 5, min=2, max=20)
 
-        @render_plotly
-        def plot1():
-            df = dat()
-            #top_sales = df.groupby('product')['quantity_ordered'].nlargest(5).reset_index()
-            top_sales = df.groupby('product')['quantity_ordered'].sum().nlargest(input.n()).reset_index() 
-            fig = px.histogram(top_sales, x='product', y="quantity_ordered", title=f"Top {input.n()} Products by Quantity Ordered")
-            return fig
-with ui.navset_card_underline(id="tab"):  
-    with ui.nav_panel("A"):
-        "Panel A content"
+#         @render_plotly
+#         def plot1():
+#             df = dat()
+#             #top_sales = df.groupby('product')['quantity_ordered'].nlargest(5).reset_index()
+#             top_sales = df.groupby('product')['quantity_ordered'].sum().nlargest(input.n()).reset_index() 
+#             fig = px.histogram(top_sales, x='product', y="quantity_ordered", title=f"Top {input.n()} Products by Quantity Ordered")
+#             return fig
+with layout_column_wrap(width=1/2):
+    with ui.navset_card_underline(id="tab", footer=ui.input_numeric("n", "Number of Items", 5, min=2, max=20)):  
+        with ui.nav_panel("Top Selling Products"):
+            @render_plotly
+            def plot_top_sellers():
+                df = dat()
+                #top_sales = df.groupby('product')['quantity_ordered'].nlargest(5).reset_index()
+                top_sales = df.groupby('product')['quantity_ordered'].sum().nlargest(input.n()).reset_index() 
+                fig = px.bar(top_sales, x='product', y="quantity_ordered", title=f"Top {input.n()} Products by Quantity Ordered")
+                return fig
 
-    with ui.nav_panel("B"):
-        "Panel B content"
+        with ui.nav_panel("Top Seller Value($)"):
+            @render_plotly
+            def plot_top_sellers_value():
+                df = dat()
+                #top_sales = df.groupby('product')['quantity_ordered'].nlargest(5).reset_index()
+                top_sales = df.groupby('product')['value'].sum().nlargest(input.n()).reset_index() 
+                fig = px.bar(top_sales, x='product', y="value", title=f"Top {input.n()} Products by Value")
+                return fig
 
-    with ui.nav_panel("C"):
-        "Panel C content"
+        with ui.nav_panel("Lowest Sellers"):
+            @render_plotly
+            def plot_lowest_sellers():
+                df = dat()
+                #top_sales = df.groupby('product')['quantity_ordered'].nlargest(5).reset_index()
+                top_sales = df.groupby('product')['quantity_ordered'].sum().nsmallest(input.n()).reset_index() 
+                fig = px.bar(top_sales, x='product', y="quantity_ordered", title=f"Top {input.n()} Products by Quantity Ordered")
+                return fig
 
-    with ui.nav_menu("Other links"):
-        with ui.nav_panel("D"):
-            "Page D content"
+        with ui.nav_panel("Lowest Seller Value($)"):
+            @render_plotly
+            def plot_lowest_sellers_value():
+                df = dat()
+                #top_sales = df.groupby('product')['quantity_ordered'].nlargest(5).reset_index()
+                top_sales = df.groupby('product')['value'].sum().nsmallest(input.n()).reset_index() 
+                fig = px.bar(top_sales, x='product', y="value", title=f"Top {input.n()} Products by Value")
+                return fig
+            
+    with ui.card():
+        ui.card_header("Sales by Time of Day Heatmap")
+        "Heatmap"
 
-        "----"
-        "Description:"
-        with ui.nav_control():
-            ui.a("Shiny", href="https://shiny.posit.co", target="_blank")
+with ui.card():
+    ui.card_header("Sales by location Map")
+    "Content Here"
 
 with ui.card():
     ui.card_header("Sales by City in 2023")
@@ -90,8 +119,6 @@ with ui.card():
                 category_orders={"month": month_order},
             )
             return fig
-
-
 
 with ui.card():
     ui.card_header("Sample Sales Data")
